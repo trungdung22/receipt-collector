fd6c2ccc2f8171a4708880415c3810f44cdc2d94
dungdo-wizeline <86394154+dungdo-wizeline@users.noreply.github.com>
2022-03-17 16:53:48 +0700

    remove cors spring security

--- src/main/java/com/demo/restapi/Application.java
+++ src/main/java/com/demo/restapi/Application.java
@@ -7,7 +7,8 @@ import org.modelmapper.ModelMapper;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.convert.Jsr310Converters;
-
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import javax.annotation.PostConstruct;
 import java.util.TimeZone;
 
@@ -32,5 +33,21 @@ public class Application {
     @Bean
     public ModelMapper modelMapper() {
         return new ModelMapper();
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                final long MAX_AGE_SECS = 3600;
+
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedHeaders("*")
+                        .maxAge(MAX_AGE_SECS);
+            }
+        };
     }
 }
--- src/main/java/com/demo/restapi/config/SecurityConfig.java
+++ src/main/java/com/demo/restapi/config/SecurityConfig.java
@@ -42,8 +42,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
-        http.cors().and().csrf().disable()
-                .exceptionHandling()
+        http.exceptionHandling()
                 .authenticationEntryPoint(unauthorizedHandler)
                 .and()
                 .sessionManagement()
